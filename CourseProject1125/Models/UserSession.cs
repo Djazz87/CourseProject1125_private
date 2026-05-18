9de7d6be83a7b2a7566273e2e09489b8a88d4e32
NorthVaryag <olegklimuk2008@gmail.com>
2026-05-18 11:28:31 +1000

    /

---
+++ CourseProject1125/Models/UserSession.cs
@@ -0,0 +1,8 @@
+namespace CourseProject1125.Models;
+
+public static class UserSession
+{
+    public static int UserId { get; set; }
+    public static string UserName { get; set; }
+    public static string Role  { get; set; }
+}
